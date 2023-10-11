689714a67c9dd05abe6a6de204b0b8e2bd6becc8
zurika777@gmail.com <zurika777@gmail.com>
2023-10-11 23:22:30 +0400

    daemata gulpi

---
+++ js/general.js
@@ -0,0 +1,14 @@
+let formState = false;
+const openForm = () => {
+  let form = document.getElementById('createForm');
+  let button = document.getElementById('openForm');
+  if (!formState) {
+    form.style.display= 'flex';
+    formState = true;
+    button.innerText = 'ფორმის დახურვა';
+  }else{
+    form.style.display = 'none';
+    formState = false;
+    button.innerText = 'ფორმის გახსნა';
+  }
+}
