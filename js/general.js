1394348a4f960cf625a400b48066c21e3623879f
zurika777 <44270254+zurika777@users.noreply.github.com>
2023-10-11 23:35:39 +0400

    butoni gasworda

--- js/general.js
+++ js/general.js
@@ -1,14 +1,16 @@
+
 let formState = false;
+
 const openForm = () => {
-  let form = document.getElementById('createForm');
-  let button = document.getElementById('openForm');
-  if (!formState) {
-    form.style.display= 'flex';
-    formState = true;
-    button.innerText = 'ფორმის დახურვა';
-  }else{
-    form.style.display = 'none';
-    formState = false;
-    button.innerText = 'ფორმის გახსნა';
-  }
+    let form = document.getElementById('createForm');
+    let button = document.getElementById('openForm');
+    if(!formState){
+        form.style.display = "flex";
+        formState = true;
+        button.innerText= "ფორმის დახურვა";
+    }else {
+        form.style.display = "none";
+        formState = false;
+        button.innerText= "ფორმის გახსნა";
+    }
 }
