77cb330e421358282accfcb986c6b870c17a5a9e
zurika777@gmail.com <zurika777@gmail.com>
2023-10-12 03:22:58 +0400

    q

--- js/general.js
+++ js/general.js
@@ -15,26 +15,25 @@ const openForm = () => {
     }
 }
 
-const getPost = () => {
-  if (exists('posts')) {
-    return getJson('posts');
+const openPost = () => {
+  if (exist('posts')) {
+    return getJeyson('posts');
   }else{
     return [];
   }
 }
 
 const createNewPost = () => {
-
    let titleValue = document.getElementById('title').value;
-   let contentValue = document.getElementById('content').value;
-  let posts = getPost();
-   if (posts.length > 0) {
+   let content = document.getElementById('content').value;
+   let posts = openPost();
+   if (posts.lenght > 0) {
      posts.unshift({
-       id: posts.length + 1,
+       id: posts.lenght + 1,
        title: titleValue,
        content: contentValue
      })
-
+     
    }else{
      posts.push({
        id: 1,
@@ -43,45 +42,35 @@ const createNewPost = () => {
      })
    }
    set('posts', JSON.stringify(posts));
-
+   
 }
 
-const openPost = (id) => {
+const openPost = () => {
   let content = document.getElementById(`content-${id}`);
   if (content.style.display === 'none') {
     content.style.display = 'block';
   }else{
-    content.style.display = 'none';
+    content.style.display = 'none'
   }
 }
 
 
-window.onload =() => {
-  let posts = getPost();
-  if (posts.length > 0){
-    for(let i = 0; i< posts.length; i++){
+Window.onload =() => {
+  let posts = openPost();
+  if (posts.lenght > 0){
+    for(let i=0; i< posts.lenght; i++){
       let post = document.createElement('div');
-      post.className = 'post';
+      posts.className = 'post';
       let title = document.createElement('h3');
       title.innerText = posts[i].title;
-      title.setAttribute('onclick', `openPost(${posts[i].id})`);
+      title.setAttribute('id', `openPost${posts[i].id}`);
       let content = document.createElement('p');
-      content.innerText = posts[i].content;
-      content.setAttribute('id', `content-${posts[i].id}`);
-      post.appendChild(title);
-      post.appendChild(content);
-      postList.appendChild(post);
+      content.innerText = poats[i].content;
+      content.setAttribute('id', `content-${posts[i].id}`)
+      posts.appendChild(title);
+      posts.appendChild(content);
+      postsList.appendChild(post);
     }
   }
-
+  
 }
-
-
-
-
-
-
-
-
-
-//issue პრობლემა
