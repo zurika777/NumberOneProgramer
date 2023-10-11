ce731a6c4dea9b8e28d2c2cd6806fa320fccb060
zurika777@gmail.com <zurika777@gmail.com>
2023-10-12 02:45:07 +0400

    shecdoma naxe

--- js/general.js
+++ js/general.js
@@ -14,3 +14,63 @@ const openForm = () => {
         button.innerText= "ფორმის გახსნა";
     }
 }
+
+const openPost = () => {
+  if (exist('posts')) {
+    return getJeyson('posts');
+  }else{
+    return [];
+  }
+}
+
+const createNewPost = () => {
+   let titleValue = document.getElementById('title').value;
+   let content = document.getElementById('content').value;
+   let posts = openPost();
+   if (posts.lenght > 0) {
+     posts.unshift({
+       id: posts.lenght + 1,
+       title: titleValue,
+       content: contentValue
+     })
+     
+   }else{
+     posts.push({
+       id: 1,
+       title: titleValue,
+       content: contentValue
+     })
+   }
+   set('posts', JSON.stringify(posts));
+   
+}
+
+const openPost = () => {
+  let content = document.getElementById(`content-${id}`);
+  if (content.style.display === 'none') {
+    content.style.display = 'block';
+  }else{
+    content.style.display = 'none'
+  }
+}
+
+
+Window.onload =() => {
+  let posts = openPost();
+  if (posts.lenght > 0){
+    for(let i=0; i< posts.lenght; i++){
+      let post = document.createElement('div');
+      posts.className = 'post';
+      let title = document.createElement('h3');
+      title.innerText = posts[i].title;
+      title.setAttribute('id', `openPost${posts[i].id}`);
+      let content = document.createElement('p');
+      content.innerText = poats[i].content;
+      content.setAttribute('id', `content-${posts[i].id}`)
+      posts.appendChild(title);
+      posts.appendChild(content);
+      postsList.appendChild(post);
+    }
+  }
+  
+}
