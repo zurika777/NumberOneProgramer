b97bb4bd46fa181102e1fb2d9b1f235f9cadd277
zurika777@gmail.com <zurika777@gmail.com>
2023-10-15 00:17:04 +0400

    es ar vici vnaxot gadaacers ar gadaacers

--- js/general.js
+++ js/general.js
@@ -1,84 +1,74 @@
-
 let formState = false;
 
 const openForm = () => {
-    let form = document.getElementById('createForm');
-    let button = document.getElementById('openForm');
-    if(!formState){
-        form.style.display = "flex";
-        formState = true;
-        button.innerText= "ფორმის დახურვა";
-    }else {
-        form.style.display = "none";
-        formState = false;
-        button.innerText= "ფორმის გახსნა";
-    }
+  let form = document.getElementById('createForm');
+  let button = document.getElementById('openForm');
+  if (!formState) {
+    form.style.display = "flex";
+    formState = true;
+    button.innerText = "ფორმის დახურვა";
+  } else {
+    form.style.display = "none";
+    formState = false;
+    button.innerText = "ფორმის გახსნა";
+  }
 }
 
 const getPost = () => {
   if (exists('posts')) {
     return getJson('posts');
-  }else{
+  } else {
     return [];
   }
 }
 
 const createNewPost = () => {
-   let titleValue = document.getElementById('title').value;
-   let contentValue = document.getElementById('content').value;
-   let posts = getPost();
-   if (posts.length > 0) {
-     posts.unshift({
-       id: posts.length + 1,
-       title: titleValue,
-       content: contentValue
-     })
+  let titleValue = document.getElementById('title').value;
+  let contentValue = document.getElementById('content').value;
+  let posts = getPost();
+  if (posts.length > 0) {
+    posts.unshift({
+      id: posts.length + 1,
+      title: titleValue,
+      content: contentValue
+    })
 
-   }else{
-     posts.push({
-       id: 1,
-       title: titleValue,
-       content: contentValue
-     })
-   }
-   set('posts', JSON.stringify(posts));
+  } else {
+    posts.push({
+      id: 1,
+      title: titleValue,
+      content: contentValue
+    })
+  }
+  set('posts', JSON.stringify(posts));
 
 }
 
 const openPost = (id) => {
   let content = document.getElementById(`content-${id}`);
-  if (content.style.display === 'none' || !content.style.display) {
+  if (content.style.display === 'none') {
     content.style.display = 'block';
-      console.log(content.style.display)
-  }else{
+  } else {
     content.style.display = 'none'
   }
 }
 
-const createPostHTML  = (value) => {
-let post = document.createElement('div');
+window.onload = () => {
+  let posts = getPost();
+  if (posts.length > 0) {
+    for (let i = 0; i < posts.length; i++) {
+      let post = document.createElement('div');
       post.className = 'post';
       let title = document.createElement('h3');
-      title.innerText = value.title;
-      title.setAttribute('onclick', `openPost(${value.id})`);
+      title.innerText = posts[i].title;
+      title.setAttribute('onclick', `openPost(${posts[i].id})`);
       let content = document.createElement('p');
-      content.innerText = value.content;
-      content.setAttribute('id', `content-${value.id}`)
+      content.innerText = posts[i].content;
+      content.setAttribute('id', `content-${posts[i].id}`)
       post.appendChild(title);
       post.appendChild(content);
       postList.appendChild(post);
-}
-
-window.onload =() => {
-  let posts = getPost();
-  if (posts.length > 0){
-    for(let i=0; i< posts.length; i++){
-        createPostHTML(posts[i]);
     }
-  }else {
-      let post = document.createElement('h3');
-      post.innerText = " პოსტები არ არის";
-      postList.append(post);
   }
 
 }
