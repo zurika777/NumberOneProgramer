717f4a5d6155c682a9ab53ea8f266e05e46907ca
zurika777 <44270254+zurika777@users.noreply.github.com>
2024-11-19 22:57:10 +0400

    naxe

--- js/general.js
+++ js/general.js
@@ -1,23 +1,24 @@
 let formState = false;
 let path = "https://raw.githubusercontent.com/zurika777/NumberOneProgramer/main/data/lang.json";
-let language = "ka";
+let language = 'ka';
 let langs = [];
-document.getElementById('openForm').innerText = "ფორმის გახსნა";
 
-const openForm = () => {
-    let form = document.getElementById("createForm");
-    let button = document.getElementById("openForm");
+const openForm =() => {
+
+    let form = document.getElementById('createForm');
+    let button  = document.getElementById("openForm");
     if(!formState){
         form.style.display = "flex";
         formState = true;
         button.innerText = langs[language].closeForm;
-    }else{
+    }else {
         form.style.display = "none";
         formState = false;
-        button.innerText = langs[language].openForm ;
+        button.innerText = langs[language].openForm;
     }
 }
-const getPost = () => {
+
+const getPosts = () => {
     if(exists("posts")){
         return getJson("posts");
     }else{
@@ -28,19 +29,19 @@ const getPost = () => {
 const createNewPost = () => {
     let titleValue = document.getElementById('title').value;
     let contentValue = document.getElementById('content').value;
-    let posts = getPost();
+    let posts = getPosts();
     if(posts.length > 0){
         posts.unshift({
-            id: posts.length +1,
+            id: posts.length + 1,
             title: titleValue,
-            content: contentValue
+            constent: contentValue
         });
         createNewHtml({
             id: 1,
             title: titleValue,
-            content: contentValue
-        })
-    }else {
+            constent: contentValue
+        });
+    }else{
         posts.push({
             id: 1,
             title: titleValue,
@@ -49,93 +50,78 @@ const createNewPost = () => {
         createNewHtml({
             id: posts.length +1,
             title: titleValue,
-            content: contentValue
+            constent: contentValue
         })
     }
     set("posts", JSON.stringify(posts));
-    document.getElementById('title').value="";
-    document.getElementById('content').value="";
+    document.getElementById('title').value='';
+    document.getElementById('content').value='';
     openForm();
+
 }
-
-
 const openPost = (id) => {
     let content = document.getElementById(`content-${id}`);
-    console.log(content);
     if(content.style.display === "none" || !content.style.display){
         content.style.display = "block";
-    }else {
+    }else{
         content.style.display = "none";
     }
 }
 
-
 const createNewHtml = (value, top = false) => {
-    let noPosts = document.getElementById('noPosts');
+    let noPosts = document.getElementById("noPosts");
     if(noPosts){
         noPosts.remove();
     }
-        let post = document.createElement('div');
-        post.className = "post";
-        let title = document.createElement('h3');
-        title.setAttribute("onclick", `openPost(${value.id})`);
-        title.innerText = value.title;
-        let content = document.createElement('p');
-        content.setAttribute("id", `content-${value.id}`);
-        content.innerText = value.content;
-        post.appendChild(title);
-        post.appendChild(content);
+    let post = document.createElement('div');
+    post.className = "post";
+    let title = document.createElement('h3');
+    title.setAttribute('onclick', `openPost(${value.id})`);
+    title.innerText = value.title;
+    let content = document.createElement('p');
+    content.setAttribute('id', `content-${value.id}`);
+    content.innerText = value.content;
+    post.appendChild(title);
+    post.appendChild(content);
     if(top){
-
         postList.append(post);
     }else{
         postList.prepend(post);
     }
 }
 
-
-
 const swichLanguage = () => {
     if(language === "ka"){
-        language = "en"
-        document.getElementById('swichLanguage').innerText = "ქართული";
+        language ="en";
+        document.getElementById('swichLanguage').innerText ="ქართული";
     }else{
-        language = "ka"
-        document.getElementById('swichLanguage').innerText = "English"
+        language = 'ka';
+         document.getElementById('swichLanguage').innerText ="English";
     }
     document.getElementById('openForm').innerText = langs[language].openForm;
     document.getElementById('noPosts').innerText = langs[language].noPosts;
-
-
-
 }
 
-
-const pageOnload =  async () => {
-    let fetchData = await fetch(path).then((response) => {
+const pageOnload = async  () => {
+    let fetchdata = await fetch(path).then((response) => {
         return response.json();
     });
-    if(fetchData){
-        langs = fetchData;
+    if(fetchdata){
+        langs = fetchdata;
     }
-    let posts = getPost();
+    let posts = getPosts();
     if(posts.length > 0){
-        for(let i=0; i< posts.length; i++){
+        for(let i = 0; i< posts.length; i++){
             createNewHtml(posts[i], true);
         }
-    }else {
-        let post = document.createElement('div');
-        post.setAttribute("id", "noPosts");
-        post.innerText = langs[language].noPosts;
-        postList.prepend(post);
     }
-
+    let post = document.createElement('h3');
+    post.setAttribute("id", "noPosts");
+    post.innerText = langs[language].noPosts;
+    postList.prepend(post);
 }
 
 window.onload = pageOnload();
-
-
-
 
 /*let formState = false;
 let path = "https://raw.githubusercontent.com/zurika777/NumberOneProgramer/main/data/lang.json";
